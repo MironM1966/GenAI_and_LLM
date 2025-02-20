8cf2a0b8309ae3f64edd1c150a3e63558d0da0cc
MironM1966 <mmaevsky@gmail.com>
2025-02-20 11:30:02 +0200

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.title('Welcome to My Streamlit App')
-st.write('This is a simple Streamlit app.')
+st.title('Welcome to My Streamlit App for OpenAI Chat')
+st.markdown("""
+This is a sample markdown
+""")
